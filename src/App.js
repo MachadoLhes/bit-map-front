65c6f145af39f75dd5f4edde9eec0bf85b117009
Lucas Noblat <lucas.noblat@b2wdigital.com>
2018-09-13 13:43:10 -0300

    Integração inicial com o D3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Graph } from 'react-d3-graph';
+import mock_nodes from './mock_nodes.json';
+import mock_relations from './mock_relations.json';
 
+const getNodes = function() {
+  return mock_nodes.map(node => {
+    return {'id': node.name}
+  })
+};
+
+const getLinks = function() {
+  return mock_relations.map(relation => {
+    return {'source': relation.source, 'target': relation.target}
+  })
+};
+
+const data = {
+  nodes: getNodes(),
+  links: getLinks()
+};
+
+// the graph configuration, you only need to pass down properties
+// that you want to override, otherwise default ones will be used
+const myConfig = {
+  nodeHighlightBehavior: true,
+  node: {
+      color: 'red',
+      size: 1000,
+      highlightStrokeColor: 'blue'
+  },
+  link: {
+      highlightColor: 'lightblue'
+  }
+};
+
+// graph event callbacks
+const onClickNode = function(nodeId) {
+  window.alert(`Clicked node ${nodeId}`);
+};
+
+const onMouseOverNode = function(nodeId) {
+  
+};
+
+const onMouseOutNode = function(nodeId) {
+ 
+};
+
+const onClickLink = function(source, target) {
+  window.alert(`Clicked link between ${source} and ${target}`);
+};
+
+const onMouseOverLink = function(source, target) {
+  
+};
+
+const onMouseOutLink = function(source, target) {
+  
+};
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Graph
+          id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+          data={data}
+          config={myConfig}
+          onClickNode={onClickNode}
+          onClickLink={onClickLink}
+          onMouseOverNode={onMouseOverNode}
+          onMouseOutNode={onMouseOutNode}
+          onMouseOverLink={onMouseOverLink}
+          onMouseOutLink={onMouseOutLink}
+        />;
       </div>
     );
   }
