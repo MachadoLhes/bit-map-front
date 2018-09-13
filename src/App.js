7d62726fce1255a4e01f8265a9dbf8a85987e153
Reinaldo Moraes <reinaldo.moraes@b2wdigital.com>
2018-09-13 15:25:50 -0300

    Modelo de Header e Sidebar

--- src/App.js
+++ src/App.js
@@ -1,82 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Graph } from 'react-d3-graph';
-import mock_nodes from './mock_nodes.json';
-import mock_relations from './mock_relations.json';
+import { Container, Row, Col } from 'reactstrap';
+import Body from "./components/Body";
+import SideBar from './components/SideBar';
 
-const getNodes = function() {
-  return mock_nodes.map(node => {
-    return {'id': node.id, 'name': node.name}
-  })
-};
 
-const getLinks = function() {
-  return mock_relations.map(relation => {
-    return {'source': relation.source, 'target': relation.target}
-  })
-};
-
-const data = {
-  nodes: getNodes(),
-  links: getLinks()
-};
-
-// the graph configuration, you only need to pass down properties
-// that you want to override, otherwise default ones will be used
-const myConfig = {
-  nodeHighlightBehavior: true,
-  node: {
-      color: 'gray',
-      size: 1000,
-      highlightStrokeColor: 'blue',
-      symbolType: 'square',
-      fontSize: 16,
-      labelProperty: 'name',
-  },
-  link: {
-      highlightColor: 'lightblue'
-  }
-};
-
-// graph event callbacks
-const onClickNode = function(nodeId) {
-  window.alert(`Clicked node ${nodeId}`);
-};
-
-const onMouseOverNode = function(nodeId) {
-  
-};
-
-const onMouseOutNode = function(nodeId) {
- 
-};
-
-const onClickLink = function(source, target) {
-  window.alert(`Clicked link between ${source} and ${target}`);
-};
-
-const onMouseOverLink = function(source, target) {
-  
-};
-
-const onMouseOutLink = function(source, target) {
-  
-};
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Graph
-          id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
-          data={data}
-          config={myConfig}
-          onClickNode={onClickNode}
-          onClickLink={onClickLink}
-          onMouseOverNode={onMouseOverNode}
-          onMouseOutNode={onMouseOutNode}
-          onMouseOverLink={onMouseOverLink}
-          onMouseOutLink={onMouseOutLink}
-        />;
+
+        <SideBar />
+        <Body />
+
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = props => {
+  const commit = props.commit || null;
+  return (
+    <div className="header">
+
+    </div>
+  );
+};
+
+export default Header;
