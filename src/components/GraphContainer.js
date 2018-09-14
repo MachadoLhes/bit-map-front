a0e489cdda502654f533ed2e03e8335778ed530c
Reinaldo Moraes <reinaldo.moraes@b2wdigital.com>
2018-09-14 09:27:00 -0300

    Ajutes finais para apresentação

--- src/components/GraphContainer.js
+++ src/components/GraphContainer.js
@@ -44,21 +44,45 @@ class GraphContainer extends Component {
                                     <p className="card-title">Name: {this.state.selectedNode.name}</p>
 
                                     <div className="card-body">
+                                        
                                         <p className="card-item">Version: {this.state.selectedNode.version}</p>
+                                        
                                         <p className="card-item">Group: {this.state.selectedNode.group}</p>
+                                        
                                         <p className="card-item">Cpu: {this.state.selectedNode.cpu}</p>
-                                        <p>Memory: {this.state.selectedNode.memory}</p>
-                                        <p>Deploy_user: {this.state.selectedNode.deploy_user}</p>
-                                        <p>Last_deploy: {this.state.selectedNode.last_deploy}</p>
-                                        <p>Owner: {this.state.selectedNode.owner}</p>
-                                        <p>Team: {this.state.selectedNode.team}</p>
-                                        <p>Status: {this.state.selectedNode.status}</p>
-                                        <p>Raml_uri: {this.state.selectedNode.raml_uri}</p>
-                                        <p>New_relic: {this.state.selectedNode.new_relic}</p>
-                                        <p>Ambiente_deploy: {this.state.selectedNode.ambiente_deploy}</p>
-                                        <p>
-                                            uri: {this.state.selectedNode.uri}
-                                            <CustomInput />
+                                        
+                                        <p className="card-item">Memory: {this.state.selectedNode.memory}</p>
+                                        
+                                        <p className="card-item">Deploy_user: {this.state.selectedNode.deploy_user}</p>
+                                        
+                                        <p className="card-item">Last_deploy: {this.state.selectedNode.last_deploy}</p>
+                                        
+                                        <p className="card-item">
+                                            Owner: {this.state.selectedNode.owner}
+                                        </p>
+
+                                        <p className="card-item">
+                                            Team: {this.state.selectedNode.team}
+                                        </p>
+
+                                        <p className="card-item">
+                                            Status: {this.state.selectedNode.status}
+                                        </p>
+
+                                        <p className="card-item">
+                                            Raml_uri: {this.state.selectedNode.raml_uri}
+                                        </p>
+                                        
+                                        <p className="card-item">
+                                            New_relic: {this.state.selectedNode.new_relic}
+                                        </p>
+
+                                        <p className="card-item">
+                                            Ambiente_deploy: {this.state.selectedNode.ambiente_deploy}
+                                        </p>
+                                        
+                                        <p className="card-item">
+                                            URI: {this.state.selectedNode.uri} 
                                         </p>
                                     </div>
                                 </div>
